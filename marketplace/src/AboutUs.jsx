be782c0979557ff1decf4fdcec4950bb9b989f6f
Peter Quinn <peterdquinnsr@gmail.com>
2023-08-01 16:50:51 -0700

    updated links

--- marketplace/src/AboutUs.jsx
+++ marketplace/src/AboutUs.jsx
@@ -5,8 +5,14 @@ function AboutUs() {
   return (
     <div className="footer-section">
       <h4>About Us</h4>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet lacus enim.</p>
-    </div>
+      <p>At Financial Marketplace, we are committed to empowering our users with real-time data and insightful information. Our platform is designed to provide you with the tools you need to make informed financial decisions. We believe in the power of technology to transform the financial landscape, making it more accessible, transparent, and efficient.</p>
+
+      <h4>Our Mission</h4>
+      <p>Our mission is to democratize finance for all. We're building a platform where everyone has access to the financial markets, not just the wealthy. We aim to provide an intuitive and seamless experience for users of all levels, whether you're a seasoned investor or just getting started.</p>
+
+      <h4>Contact Us</h4>
+      <p>We'd love to hear from you! If you have any questions or feedback, please feel free to reach out to us at support@financialmarketplace.com. Our dedicated customer service team is always ready to assist you.</p>
+      </div>
   );
 }
 
--- marketplace/src/SocialLinks.jsx
+++ marketplace/src/SocialLinks.jsx
@@ -1,14 +1,21 @@
-// SocialLinks.jsx
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function SocialLinks() {
   return (
     <div className="footer-section">
       <h4>Follow Us</h4>
       <div className="social-links">
-        <a href="https://facebook.com"><img src="/images/facebook-icon.png" alt="Facebook" /></a>
-        <a href="https://twitter.com"><img src="/images/twitter-icon.png" alt="Twitter" /></a>
-        <a href="https://instagram.com"><img src="/images/instagram-icon.png" alt="Instagram" /></a>
+        <a href="https://facebook.com" className="facebook social">
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+        <a href="https://twitter.com" className="twitter social">
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+        <a href="https://instagram.com" className="instagram social">
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
         {/* Add more social links as needed */}
       </div>
     </div>
